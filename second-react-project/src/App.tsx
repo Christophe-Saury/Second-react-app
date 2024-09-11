b278470f945477af1535512c75f5e9c5e0f29a38
Christophe SAURY <christophesaury23@gmail.com>
2024-09-11 21:04:51 +0200

    added sidebar

--- second-react-project/src/App.tsx
+++ second-react-project/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App() {
   return (
---
+++ second-react-project/src/components/Sidebar/index.tsx
@@ -0,0 +1,32 @@
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo-sub.png'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelopeOpen,
+  faHome,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => {
+  ;<div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+    </Link>
+    <nav>
+      <NavLink to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink className="contact-link" to="/contact">
+        <FontAwesomeIcon icon={faEnvelopeOpen} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+}
+
+export default Sidebar
