560855f8044b7cd689b337addb5188d51652945c
Christophe SAURY <christophesaury23@gmail.com>
2024-09-11 21:23:25 +0200

    fixed wrong syntax

--- second-react-project/src/components/Sidebar/index.tsx
+++ second-react-project/src/components/Sidebar/index.tsx
@@ -10,23 +10,25 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
-  ;<div className="nav-bar">
-    <Link className="logo" to="/">
-      <img src={LogoS} alt="logo" />
-      <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-    </Link>
-    <nav>
-      <NavLink to="/">
-        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-      </NavLink>
-      <NavLink className="about-link" to="/about">
-        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-      </NavLink>
-      <NavLink className="contact-link" to="/contact">
-        <FontAwesomeIcon icon={faEnvelopeOpen} color="#4d4d4e" />
-      </NavLink>
-    </nav>
-  </div>
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav>
+        <NavLink to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink className="contact-link" to="/contact">
+          <FontAwesomeIcon icon={faEnvelopeOpen} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+    </div>
+  )
 }
 
 export default Sidebar
