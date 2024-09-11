9a0118dbbc718a66df18192cfd55bd27dd8d3a5f
Christophe SAURY <christophesaury23@gmail.com>
2024-09-11 21:19:55 +0200

    solved wrong link

--- second-react-project/src/components/Sidebar/index.tsx
+++ second-react-project/src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo-sub.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
