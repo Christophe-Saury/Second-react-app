b8d3ed5780c8d6a354fb4004ca27fa0a8e01552b
Christophe SAURY <christophesaury23@gmail.com>
2024-09-12 10:53:51 +0200

    added links to git and linkedin accounts

--- second-react-project/src/components/Sidebar/index.tsx
+++ second-react-project/src/components/Sidebar/index.tsx
@@ -5,9 +5,11 @@ import { Link, NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faEnvelopeOpen,
+  faGift,
   faHome,
   faUser,
 } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => {
   return (
@@ -27,6 +29,26 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faEnvelopeOpen} color="#4d4d4e" />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Christophe-Saury"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/christophe-saury-fr-usa/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
