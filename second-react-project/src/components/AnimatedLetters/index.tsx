d132aed7ffc22a069f0b72b21989df38541eb42b
Christophe SAURY <christophesaury23@gmail.com>
2024-09-12 19:06:56 +0200

    Added AnimatedLetters

---
+++ second-react-project/src/components/AnimatedLetters/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './index.scss'
+
+interface Props {
+  letterClass: string
+  strArray: string[]
+  idx: number
+}
+
+const AnimatedLetters = ({ letterClass, strArray, idx }: Props) => {
+  return (
+    <span>
+      {strArray.map((char, i) => (
+        <span key={char + i} className={`${letterClass} _${i + idx}`}>
+          {char}
+        </span>
+      ))}
+    </span>
+  )
+}
+
+export default AnimatedLetters
