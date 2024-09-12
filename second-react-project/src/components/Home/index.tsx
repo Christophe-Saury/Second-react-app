e9bba97f0051e06e04220767dc3950af20d1155f
Christophe SAURY <christophesaury23@gmail.com>
2024-09-12 14:07:29 +0200

    changed the style of the homepage

--- second-react-project/src/components/Home/index.tsx
+++ second-react-project/src/components/Home/index.tsx
@@ -6,17 +6,19 @@ import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div className="container home-page">
-      <h1>
-        Hi, <br></br>I'm
-        <img src={LogoTitle} alt="developer" />
-        Lobodan
-        <br></br>
-        web developer
-      </h1>
-      <h2>Frontend developer / Javascript developer</h2>
-      <Link to="/contact" className="flat-button">
-        Contact me
-      </Link>
+      <div className="text-zone">
+        <h1>
+          Hi, <br></br>I'm
+          <img src={LogoTitle} alt="developer" />
+          Lobodan
+          <br></br>
+          web developer
+        </h1>
+        <h2>Frontend developer / Javascript developer</h2>
+        <Link to="/contact" className="flat-button">
+          Contact me
+        </Link>
+      </div>
     </div>
   )
 }
