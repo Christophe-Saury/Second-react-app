2af9acd7c36863b3583281e4874e6a6ac99edbf5
Christophe SAURY <christophesaury23@gmail.com>
2024-09-12 13:45:49 +0200

    Added Home Page

--- second-react-project/src/App.tsx
+++ second-react-project/src/App.tsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import './App.scss'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
       </Routes>
     </>
   )
---
+++ second-react-project/src/components/Home/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import '../Home/index.scss'
+import LogoTitle from '../../assets/images/logo-s.png'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <h1>
+        Hi, <br></br>I'm
+        <img src={LogoTitle} alt="developer" />
+        Lobodan
+        <br></br>
+        web developer
+      </h1>
+      <h2>Frontend developer / Javascript developer</h2>
+      <Link to="/contact" className="flat-button">
+        Contact me
+      </Link>
+    </div>
+  )
+}
+
+export default Home
