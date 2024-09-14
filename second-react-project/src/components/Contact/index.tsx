cc4c57a7c36833186fa560592756ceb3dc244d21
Christophe SAURY <christophesaury23@gmail.com>
2024-09-14 12:08:03 +0200

    added contact page

---
+++ second-react-project/src/components/Contact/index.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react'
+import './index.scss'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+    return () => clearTimeout(timeoutId)
+  }, [])
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>Feel Free to contact me, i am open to new opportunities.</p>
+
+          <div className="contact-form">
+            <form>
+              <ul>
+                <li className="half">
+                  <input type="text" name="name" placeholder="Name" required />
+                </li>
+                <li className="half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea name="message" placeholder="Message" required />
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+
+      <Loader type="pacman" active={true} />
+    </>
+  )
+}
+
+export default Contact
